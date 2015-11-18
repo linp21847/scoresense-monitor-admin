67615c66588e80f6a9f2d47c7be49d2c8b7d6a0b
Paul <linp21847@gmail.com>
2015-11-18 15:55:03 +0100

    mysql_real_escape_string function issue fixed

--- apis/insertData.php
+++ apis/insertData.php
@@ -5,10 +5,9 @@ include '../includes/config.php';
 if (!isset($_POST) || empty($_POST)) {
 	echo json_encode(array('status' => 'bad'));
 } else {
-	$params = mysql_real_escape_string($_POST['data']);
+	$params = mysqli_real_escape_string($_POST['data']);
 	$now = date('H:i:s m-d-Y');
 	$mysqli->query("INSERT INTO `data`(`data`, `create_at`) VALUES ('{$params}','{$now}')");
-	// $insertResult = mysql_query($insertQuery);
 
 	$id = $mysqli->insert_id;
 	echo json_encode(array('status' => 'ok', 'msg' => "Successfully Added.", 'id' => $id));
--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 
 header('Access-Control-Allow-Origin: *');
 var_dump($_POST['data']);
-$params = mysql_real_escape_string($_POST['data']);
+$params = mysqli_real_escape_string($_POST['data']);
 echo $params;
 
 ?>
