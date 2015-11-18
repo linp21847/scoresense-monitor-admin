acb7c3255d575c4ed3659a6a92aba71982db11eb
Paul <linp21847@gmail.com>
2015-11-18 16:11:18 +0100

    mysqli_real_escape_string issue fixed

--- apis/insertData.php
+++ apis/insertData.php
@@ -5,7 +5,7 @@ include '../includes/config.php';
 if (!isset($_POST) || empty($_POST)) {
 	echo json_encode(array('status' => 'bad'));
 } else {
-	$params = mysqli_real_escape_string($_POST['data']);
+	$params = mysqli_real_escape_string($mysqli, $_POST['data']);
 	$now = date('H:i:s m-d-Y');
 	$mysqli->query("INSERT INTO `data`(`data`, `create_at`) VALUES ('{$params}','{$now}')");
 
